5b01c4c5113274b5102b5f13b75ec7cf162367d4
xdh <1393949909@qq.com>
2021-10-13 14:46:47 +0800

    接口

--- src/main/java/com/ccoc/controller/ApiController.java
+++ src/main/java/com/ccoc/controller/ApiController.java
@@ -1,10 +1,9 @@
 package com.ccoc.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.ccoc.dao.GoodsMapper;
-import com.ccoc.dao.SwipersMapper;
-import com.ccoc.model.Goods;
-import com.ccoc.model.Swipers;
+import com.ccoc.dao.*;
+import com.ccoc.model.*;
+import com.ccoc.util.ImgDetail;
 import com.github.pagehelper.PageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,6 +30,18 @@ public class ApiController {
     @Resource
     private GoodsMapper goodsMapper;
 
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    @Resource
+    private CategoryItemMapper categoryItemMapper;
+
+    @Resource
+    private NewsMapper newsMapper;
+
+    @Resource
+    private DetailSwipersMapper detailSwipersMapper;
+
 
     @RequestMapping(value = "/swipers/findAll", method = RequestMethod.GET)
     @ResponseBody
@@ -38,7 +50,7 @@ public class ApiController {
         response.setHeader("Access-Control-Allow-Origin", "*");
         JSONObject json = new JSONObject();
         List<Swipers> swiperses = swipersMapper.findAll();
-        json.put("message",swiperses);
+        json.put("message", swiperses);
         json.put("status", 0);
         return json;
     }
@@ -46,7 +58,7 @@ public class ApiController {
 
     @RequestMapping(value = "/getgoods", method = RequestMethod.GET)
     @ResponseBody
-    public JSONObject getgoods(HttpServletResponse response, HttpServletRequest request) {
+    public JSONObject getGoods(HttpServletResponse response, HttpServletRequest request) {
         //设置响应头
         response.setHeader("Access-Control-Allow-Origin", "*");
 
@@ -54,10 +66,10 @@ public class ApiController {
         //获取传递参数
         String pageIndex = request.getParameter("pageIndex");
         String pageSize = request.getParameter("pageSize");
-        if(StringUtils.isEmpty(pageIndex)) {
+        if (StringUtils.isEmpty(pageIndex)) {
             pageIndex = "1";
         }
-        if(StringUtils.isEmpty(pageSize)) {
+        if (StringUtils.isEmpty(pageSize)) {
             pageSize = "10";
         }
         JSONObject json = new JSONObject();
@@ -66,7 +78,96 @@ public class ApiController {
         int pageSizeF = Integer.valueOf(pageSize);
         PageHelper.startPage(pageIndexF, pageSizeF);
         List<Goods> lists = goodsMapper.findAllGoodsByPageF(pageIndexF, pageSizeF);
-        json.put("message",lists);
+        json.put("message", lists);
+        json.put("status", 0);
+        return json;
+    }
+
+    @RequestMapping(value = "/getimgcategory", method = RequestMethod.GET)
+    @ResponseBody
+    public JSONObject getImgCategory(HttpServletResponse response) {
+        //设置响应头
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        JSONObject json = new JSONObject();
+        List<Category> categories = categoryMapper.findAll();
+        json.put("message", categories);
+        json.put("status", 0);
+        return json;
+    }
+
+
+    @RequestMapping(value = "/getimages", method = RequestMethod.GET)
+    @ResponseBody
+    public JSONObject getImages(HttpServletResponse response, HttpServletRequest request) {
+        //设置响应头
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        String cateId = request.getParameter("cateId");
+        JSONObject json = new JSONObject();
+        List<CategoryItem> categoryItems = categoryItemMapper.findByCateId(cateId);
+        json.put("message", categoryItems);
+        json.put("status", 0);
+        return json;
+    }
+
+
+    @RequestMapping(value = "/getnewslist", method = RequestMethod.GET)
+    @ResponseBody
+    public JSONObject getNewsList(HttpServletResponse response) {
+        //设置响应头
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        JSONObject json = new JSONObject();
+        List<News> news = newsMapper.findAll();
+        json.put("message", news);
+        json.put("status", 0);
+        return json;
+    }
+
+    @RequestMapping(value = "/getNews", method = RequestMethod.GET)
+    @ResponseBody
+    public JSONObject getNews(HttpServletResponse response, HttpServletRequest request) {
+        String id = request.getParameter("id");
+        //设置响应头
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        JSONObject json = new JSONObject();
+        News news = newsMapper.findById(id);
+        json.put("message", news);
+        json.put("status", 0);
+        return json;
+    }
+
+
+    @RequestMapping(value = "/getDetailSwipers", method = RequestMethod.GET)
+    @ResponseBody
+    public JSONObject getDetailSwipers(HttpServletResponse response, HttpServletRequest request) {
+        String pid = request.getParameter("pid");
+        //设置响应头
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        JSONObject json = new JSONObject();
+        List<DetailSwipers> detailSwipers = detailSwipersMapper.findByPId(pid);
+        if (detailSwipers != null && detailSwipers.size() > 0) {
+
+            List<ImgDetail> list = new ArrayList<>();
+            for (DetailSwipers swipers : detailSwipers) {
+                ImgDetail imgDetail = new ImgDetail();
+                imgDetail.setSrc(swipers.getSrc());
+                list.add(imgDetail);
+            }
+            json.put("message", list);
+            json.put("status", 0);
+            return json;
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/getGoodsById", method = RequestMethod.GET)
+    @ResponseBody
+    public JSONObject getGoodsById(HttpServletResponse response, HttpServletRequest request) {
+        String id = request.getParameter("id");
+        //设置响应头
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        JSONObject json = new JSONObject();
+        Goods goods = goodsMapper.findById(id);
+        json.put("message", goods);
         json.put("status", 0);
         return json;
     }
---
+++ src/main/java/com/ccoc/util/ImgDetail.java
@@ -0,0 +1,17 @@
+package com.ccoc.util;
+
+/**
+ * Created by pc on 2021/10/13.
+ */
+public class ImgDetail {
+
+    private String src;
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+}
